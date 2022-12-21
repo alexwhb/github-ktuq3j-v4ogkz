d235708311086050ed72fdcf4437e1063704fc21
alexwhb <alexwhb@gmail.com>
2022-12-21 13:14:42 -0800

    1 file modified

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,29 +1,37 @@
 import { NextPage } from 'next';
+import Head from 'next/head';
 
 const Login: NextPage = () => {
   return (
-    <main className="flex content-center justify-center h-screen items-center ">
-      <div className="w-96 p-4 rounded bg-white ">
-        <h1 className="text-3xl text-center font-semibold">Login</h1>
+    <div>
+      <Head>
+        <title>Login</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-        <form>
-          <input
-            className="bg-gray-100 p-3 mt-8 w-full rounded"
-            placeholder="username"
-          />
-          <input
-            className="bg-gray-100 p-3 mt-3 w-full rounded"
-            placeholder="password"
-          />
-          <button
-            type="submit"
-            className="bg-sky-700 w-full text-gray-100 p-2 rounded mt-8"
-          >
-            Submit
-          </button>
-        </form>
-      </div>
-    </main>
+      <main className="flex content-center justify-center h-screen items-center ">
+        <div className="w-96 p-4 rounded bg-white ">
+          <h1 className="text-3xl text-center font-semibold">Login</h1>
+
+          <form>
+            <input
+              className="bg-gray-100 p-3 mt-8 w-full rounded"
+              placeholder="username"
+            />
+            <input
+              className="bg-gray-100 p-3 mt-3 w-full rounded"
+              placeholder="password"
+            />
+            <button
+              type="submit"
+              className="bg-sky-700 w-full text-gray-100 p-2 rounded mt-8"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+      </main>
+    </div>
   );
 };
 
