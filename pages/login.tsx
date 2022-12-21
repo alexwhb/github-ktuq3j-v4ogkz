a6b6aa81c28b5886969624cb88335fcef2d5e0ab
alexwhb <alexwhb@gmail.com>
2022-12-21 13:10:23 -0800

    Updates login view

--- pages/login.tsx
+++ pages/login.tsx
@@ -7,8 +7,14 @@ const Login: NextPage = () => {
         <h1 className="text-3xl text-center font-semibold">Login</h1>
 
         <form>
-          <input className="bg-gray-100 p-3 mt-8 w-full rounded"></input>
-          <input className="bg-gray-100 p-3 mt-3 w-full rounded"></input>
+          <input
+            className="bg-gray-100 p-3 mt-8 w-full rounded"
+            placeholder="username"
+          />
+          <input
+            className="bg-gray-100 p-3 mt-3 w-full rounded"
+            placeholder="password"
+          />
           <button
             type="submit"
             className="bg-sky-700 w-full text-gray-100 p-2 rounded mt-8"
